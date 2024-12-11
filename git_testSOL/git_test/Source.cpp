83ba5c161ac58e5e4d65c5986634bed7c0b8bc95
Alberto6126 <alberto.sanchez@enti.cat>
2024-12-11 12:56:33 +0100

    Update Source.cpp

--- git_testSOL/git_test/Source.cpp
+++ git_testSOL/git_test/Source.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
+#include<string>
+
+
+
+
+std::string nombre() {
+	std::string input;
+	std::cout << "\nIntroduce tu nombre: ";
+	std::cin >> input;
+	return input; 
+}
 
 void main() {
 	std::cout << "Hello World!";
+	std::cout << "\nBienvenido " << nombre();
 }
